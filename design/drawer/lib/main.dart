f675a2f3c9cda18a069b1619aeafb219ff559bde
root <root@PC22082.elcaNet.local>
2020-11-06 16:55:33 +0700

    finish design

---
+++ design/drawer/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+    final appTitle = 'Drawer Demo';
+    @override
+    Widget build(BuildContext context){
+        return MaterialApp(
+                title: appTitle,
+                home: MyHomePage(title: appTitle),
+        );
+    }
+}
+
+class MyHomePage extends StatelessWidget{
+    
+    final String title;
+
+    MyHomePage({Key key, this.title}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context){
+        return Scaffold(
+
+                appBar: AppBar(
+
+                        title: Text(this.title),
+                ),
+                body: Center(child: Text('My Page!'),),
+                drawer: Drawer(
+                        child: ListView(
+                                padding: EdgeInsets.zero,
+                                children: [
+                                    DrawerHeader(
+                                            child: Text('Drawer Header'),
+                                            decoration: BoxDecoration(
+                                                    color: Colors.blue,
+                                            ),
+                                    ),
+                                    ListTile(
+                                            title: Text('Item 1'),
+                                            onTap: (){
+                                                Navigator.pop(context);
+                                            },
+                                    ),
+                                    ListTile(
+                                            title: Text('Item 2'),
+                                            onTap: (){
+                                                Navigator.pop(context);
+                                            },
+                                    )
+                                ],
+                        ),
+                ),
+
+        );
+    }
+}
